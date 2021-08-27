1e6fa2f0b05a1837f7643a4d8e61c07df96349fc
malt0533 <malt0533@stud.kea.dk>
2021-08-27 12:39:33 +0200

    Funktionalitet færdig

--- undervisningsopgaver/rest/bistro_babushka.js
+++ undervisningsopgaver/rest/bistro_babushka.js
@@ -16,45 +16,88 @@ var settings = {
 	},
 };
 
-async function fetchDBData() {
-	const res = await fetch(db, settings);
-	const json = await res.json();
-	vis(json);
+// DOM Elementer
+const btns = document.querySelectorAll("button");
+
+//Globale Variabler
+let retter;
+let filter = "alle";
+
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+	loadJSON();
+	// Knapper
+	btns.forEach((btn) => {
+		btn.addEventListener(
+			"click",
+			filtrerRetter
+			// (e) => {
+			// 	btns.forEach((activeState) => {
+			// 		activeState.classList.remove("active");
+			// 	});
+			// 	let clicked = e.target;
+			// 	clicked.classList.add("active");
+			// 	filter = clicked.textContent.toLowerCase();
+			// 	filtrerRetter();
+			// 	console.log(btn.dataset);
+			// });
+		);
+	});
+}
+
+// Filtrering
+function filtrerRetter() {
+	filter = this.dataset.retter;
+	document.querySelector(".active").classList.remove("active");
+	this.classList.add("active");
+
+	let firstLetter = filter.charAt(0).toUpperCase() + filter.slice(1);
+
+	document.querySelector("h2").textContent = firstLetter;
+
+	vis(retter);
+}
+
+// Rest API Call
+async function loadJSON() {
+	const JSONData = await fetch(db, settings);
+	retter = await JSONData.json();
+	vis(retter);
 }
 
 function vis(json) {
 	const retterTemplate = document.querySelector("template");
 	const container = document.querySelector("#liste");
 	let count = 0;
+	container.textContent = "";
 
 	json.forEach((el) => {
 		count++;
-		let klon = retterTemplate.cloneNode(true).content;
-		klon.querySelector(".name").textContent = el.navn;
-		klon.querySelector(".kort").textContent = el.kortbeskrivelse;
-		klon.querySelector("img").src = `faces/img${count}.jpg`;
-		klon.querySelector(
-			".country"
-		).textContent = `Land: ${el.oprindelsesregion}`;
-		// klon.querySelector(".year").textContent = `Årgang: ${el.year}`;
-		// klon.querySelector(".createdAt").textContent = `Created: ${truncateString(
-		// 	el.createdAt,
-		// 	10
-		// )}`;
 
-		//Appender alle elementerne
-		container.appendChild(klon);
+		if (filter == el.kategori || filter == "alle") {
+			let klon = retterTemplate.cloneNode(true).content;
+			klon.querySelector(".name").textContent = el.navn;
+			klon.querySelector(".kort").textContent = el.kortbeskrivelse;
+			klon.querySelector("img").src = `faces/img${count}.jpg`;
+			klon.querySelector(
+				".country"
+			).textContent = `Land: ${el.oprindelsesregion}`;
+			// klon.querySelector(".createdAt").textContent = `Created: ${truncateString(
+			// 	el.createdAt,
+			// 	10
+			// )}`;
+
+			//Appender alle elementerne
+			container.appendChild(klon);
+		}
 	});
-
-	console.log(json);
 }
 
-function truncateString(string, limit) {
-	if (string.length > limit) {
-		return string.substring(0, limit);
-	} else {
-		return string;
-	}
-}
-
-fetchDBData();
+// function truncateString(string, limit) {
+// 	if (string.length > limit) {
+// 		return string.substring(0, limit);
+// 	} else {
+// 		return string;
+// 	}
+// }
